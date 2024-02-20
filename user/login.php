dfe8d2fab03a7d379bd47655c40303ad9eaa24f1
Florian Gourinat <58781208+floriangourinat@users.noreply.github.com>
2024-02-20 15:53:33 +0100

    Connexion à l'espace administrateur
    Ce commit inclut une page de connexion permettant aux administrateurs de se connecter à l'espace administrateur du garage Vincent Parrot. Les utilisateurs peuvent saisir leur adresse e-mail et leur mot de passe pour accéder à leur compte. Le formulaire de connexion effectue une vérification sécurisée des informations saisies par rapport à celles stockées dans la base de données. En cas d'erreur d'authentification, un message d'erreur approprié est affiché.

--- user/login.php
+++ user/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../includes/db.php'; // Vérifie que ce chemin est correct
+require_once '../includes/db.php'; // Vérifie que ce chemin est correct 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
